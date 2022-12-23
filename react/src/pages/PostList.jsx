fea47d105589f999eaee02669fc41b7a63fff61a
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-12-23 19:47:39 +0400

    Make request and get posts

--- react/src/pages/PostList.jsx
+++ react/src/pages/PostList.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+
 export default function PostList() {
+  const [postList, setPostList] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(posts => {
+        setPostList(posts)
+      })
+    return () => {
+      console.log("Destroy")
+    }
+  }, [])
+  
+
+
   return (
-    <div>Post List</div>
+    <div>
+      <pre>{JSON.stringify(postList, undefined, 2)}</pre>
+    </div>
   )
 }
--- vue/src/pages/PostList.vue
+++ vue/src/pages/PostList.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    Post List
+    <pre>{{ postList }}</pre>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+
+const postList = ref([])
+
+onMounted(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => {
+      postList.value = posts
+    })
+})
 
 </script>
 
